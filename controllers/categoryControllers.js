09b34dd1d743075c0b45635f59476b64af552e57
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-09-28 12:56:17 -0300

    Avances con las categorias, y esquema de categorias y productos

---
+++ controllers/categoryControllers.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+const Category = require("../models/categorySchema")
+
+const getAllCategories = async (req, res) => {
+    const categories = await Category.find();
+    try {
+      if (!categories) {
+        return res.status(404).json({
+          mensaje: "Categorias no encontradas",
+          status: 404
+        });
+      }
+  
+      return res.status(200).json({
+        mensaje: "Categorias encontrados",
+        status: 200,
+        categories
+      });
+    } catch (error) {
+      // error de servidor 500
+      return res.status(500).json({
+        mensaje: "Hubo un error, inténtelo más tarde",
+        status: 500
+      });
+    }
+  };
+
+const createCategory = async (req, res) => {
+    const { name } = req.body;
+    const category = await Category.findOne({ name })
+
+    try {
+        if (category) {
+            return res.status(404).json({
+                mensaje: "Categoria ya existe",
+            })
+        }
+        const newCategory = new Category({ name });
+        await newCategory.save()
+
+        return res.status(201).json({
+            mensaje: "Categoria creada",
+            status: 201,
+            newCategory
+        })
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: "Hubo un error, inténtelo más tarde",
+            status: 500
+        });
+    }
+}
+
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete(id);
+    try {
+      if (!mongoose.isValidObjectId(id)) {
+        return res.status(400).json({
+          mensaje: "Id inválido",
+          status: 400,
+        });
+      }
+      if (!category) {
+        return res.status(404).json({
+          mensaje: "Categoria no encontrado",
+          status: 404,
+        });
+      }
+  
+      return res.status(200).json({
+        mensaje: "Categoria borrada correctamente",
+        status: 200,
+        category
+      });
+    } catch (error) {
+        console.log(error)
+      return res.status(500).json({
+        mensaje: "Hubo un error, inténtelo más tarde",
+        status: 500,
+      });
+    }
+  };
+
+module.exports = {
+    createCategory,
+    getAllCategories,
+    deleteCategory
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
+const { getAllCategories, createCategory, deleteCategory } = require("../controllers/categoryControllers")
 const { register, getAllUsers, changeToAdmin, getUserByID, deleteUser, login, updateUser} = require("../controllers/userController")
 
 const router = require ("express").Router()
 
-// user routes
+// User Routes
 router.get("/user", getAllUsers)
 router.get("/user/:id", getUserByID)
 router.delete("/user/:id", deleteUser)
@@ -10,5 +11,10 @@ router.put("/user/:id", updateUser)
 router.post("/register", register)
 router.post("/login", login)
 router.put("/admin/:id", changeToAdmin)
+
+//Category Routes
+router.get("/categories", getAllCategories)
+router.post("/category", createCategory)
+router.delete("/category/:id", deleteCategory)
 
 module.exports = router

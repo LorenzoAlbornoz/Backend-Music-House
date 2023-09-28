15d05b1368b962ae918a77e20084bd51f782b310
LorenzoAlbornoz <lorenzoa.28.la@gmail.com>
2023-09-28 16:58:38 -0300

    Avances en ProductControllers, CategoryControllers y ProductSchema

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,23 +1,30 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 const Category = require("./categorySchema");
 
 const productSchema = new mongoose.Schema({
     title: {
         type: String
     },
-    image:{
+    description: {
         type: String
     },
-    detail:{
-        type: String
-    },
-    price:{
-        type: String
+    price: {
+        type: Number
     },
     category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: Category
-}
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Category
+    },
+    image: {
+        type: String
+    },
+    stock: {
+        type: Number
+    },
+    favorite: {
+        type: Boolean,
+        default: false
+    }
 })
 
 const Product = mongoose.model("Product", productSchema)
